7e1b7418272f8e6a9fdd71459a99fad06cfba24a
Robert Hibbard <roblhibbard@gmail.com>
2017-09-06 21:08:12 -0600

    implemented validations for blogs, skills, and portfolio items

---
+++ notes/notes.rb
@@ -0,0 +1,5 @@
+# enum feature would be good to setup and toggle active or unactive inside mycrm
+# checkout the feature of routes | grep controller_name to only list that paticular route
+# use byebug and ask for params
+# use github to merge branch to main branch best to delete after merge
+ 
