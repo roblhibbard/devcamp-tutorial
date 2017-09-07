7220db7bf7eb1e51b95a5864040c4c6e48baa3de
Robert Hibbard <roblhibbard@gmail.com>
2017-09-06 22:35:49 -0600

    integreated database relationship between topics and blog posts

--- notes/notes.rb
+++ notes/notes.rb
@@ -2,4 +2,5 @@
 # checkout the feature of routes | grep controller_name to only list that paticular route
 # use byebug and ask for params
 # use github to merge branch to main branch best to delete after merge
- 
+# Lecture 65 at 8:52 shows how to breakdown the logical way to decide belongs_to and has_many 
+#foreign key references another table
